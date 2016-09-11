7ded622cd53afe9cb111603e90473ab817850054
gosho-kazuya <ketsume0211@gmail.com>
2016-09-11 17:42:14 +0900

    rename bin dir

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=['console_calendar/bin/cc'],
+    scripts=['bin/cc'],
     install_requires=[
         'termcolor',
     ],
-    # extras_require={
-    #     'cors': ['Flask-Cors>=2.0.0'],
-    # },
     tests_require=[
         'ipython',
         'pytest',
@@ -26,10 +23,7 @@ setup(
     author='Kazuya Gosho',
     author_email='ketsume0211@gmail.com',
     url='https://github.com/acro5piano/console_calendar',
-    # download_url=(
-    #     'https://github.com/airbnb/caravel/tarball/' + version.VERSION_STRING),
     classifiers=[
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
